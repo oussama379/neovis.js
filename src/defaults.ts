30cdda944ee0dbf5e0a116ab5d0098d120d1c27c
oussama379 <hennaneoussama@gmail.com>
2023-04-26 14:25:04 +0200

    Replace exists() function with IS NOT NULL syntax

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,64 +1,64 @@
 const defaults = {
-
-	neo4j: {
-		initialQuery: `MATCH (n) WHERE exists(n.pagerank)
+  neo4j: {
+    initialQuery: `MATCH (n) WHERE NOT NULL (n.pagerank)
                         WITH (n), RAND() AS random
                         ORDER BY random LIMIT 3000
                         OPTIONAL MATCH (n)-[r]-(m)
-                        //WITH n,r,m WHERE exists(n.pagerank) AND exists(m.pagerank) AND exists(m.community)
+                        //WITH n,r,m WHERE NOT NULL (n.pagerank) AND NOT NULL (m.pagerank) AND NOT NULL (m.community)
                         RETURN n, r, m;`,
-		neo4jUri: 'bolt://localhost:7687',
-		neo4jUser: 'neo4j',
-		neo4jPassword: 'neo4j',
-		driverConfig: {
-			encrypted: 'ENCRYPTION_OFF',
-			trust: 'TRUST_ALL_CERTIFICATES',
-			maxConnectionPoolSize: 100,
-			connectionAcquisitionTimeout: 10000,
-			disableLosslessIntegers: true,
-		}
-	},
+    neo4jUri: "bolt://localhost:7687",
+    neo4jUser: "neo4j",
+    neo4jPassword: "neo4j",
+    driverConfig: {
+      encrypted: "ENCRYPTION_OFF",
+      trust: "TRUST_ALL_CERTIFICATES",
+      maxConnectionPoolSize: 100,
+      connectionAcquisitionTimeout: 10000,
+      disableLosslessIntegers: true,
+    },
+  },
 
-	visJs: {
-		nodes: {
-			font: {
-				size: 26,
-				strokeWidth: 7
-			},
-			scaling: {}
-		},
-		edges: {
-			arrows: {
-				to: { enabled: false }
-			},
-			length: 200
-		},
-		layout: {
-			improvedLayout: false,
-			hierarchical: {
-				enabled: false,
-				sortMethod: 'hubsize'
-			}
-		},
-		physics: { // TODO: adaptive physics settings based on size of graph rendered
-			// enabled: true,
-			// timestep: 0.5,
-			// stabilization: {
-			//     iterations: 10
-			// }
+  visJs: {
+    nodes: {
+      font: {
+        size: 26,
+        strokeWidth: 7,
+      },
+      scaling: {},
+    },
+    edges: {
+      arrows: {
+        to: { enabled: false },
+      },
+      length: 200,
+    },
+    layout: {
+      improvedLayout: false,
+      hierarchical: {
+        enabled: false,
+        sortMethod: "hubsize",
+      },
+    },
+    physics: {
+      // TODO: adaptive physics settings based on size of graph rendered
+      // enabled: true,
+      // timestep: 0.5,
+      // stabilization: {
+      //     iterations: 10
+      // }
 
-			adaptiveTimestep: true,
-			// barnesHut: {
-			//     gravitationalConstant: -8000,
-			//     springConstant: 0.04,
-			//     springLength: 95
-			// },
-			stabilization: {
-				iterations: 200,
-				fit: true
-			}
-		}
-	}
+      adaptiveTimestep: true,
+      // barnesHut: {
+      //     gravitationalConstant: -8000,
+      //     springConstant: 0.04,
+      //     springLength: 95
+      // },
+      stabilization: {
+        iterations: 200,
+        fit: true,
+      },
+    },
+  },
 };
 
 export { defaults };
